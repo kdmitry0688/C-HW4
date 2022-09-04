173c9f86342f723175d8e5dc0dc7d2d3047899be
Dmitriy <kazany.dmitry@gmail.com>
2022-09-04 23:11:18 +0300

    add task 25 exponent

---
+++ Task25_Exponent/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число ");
+double Number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень, в которую необходимо возвести число ");
+double Expon = Convert.ToInt32(Console.ReadLine());
+
+double pow = Math.Pow(Number, Expon);
+
+Console.WriteLine(pow);
