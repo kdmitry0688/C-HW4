7828b8be2ec7bb1078076f0dd1a01686cb6dd0ab
Dmitriy <kazany.dmitry@gmail.com>
2022-09-05 00:33:04 +0300

    add task 29 array digitals

---
+++ Task29_ArrSumDig/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int[8];
+FillArray(array);
+
+void FillArray(int[] array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1,100);
+        if (i==0)
+            Console.Write("[ " + array[i] + " , ");
+        else if (i >= 1 && i <= size-2)
+            Console.Write(array[i] + " , ");
+        else if (i == size-1)
+            Console.Write(array[i] + " ]");
+    }
+}
