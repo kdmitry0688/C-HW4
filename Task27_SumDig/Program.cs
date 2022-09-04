a28f0c077a89b20908a491e795d1910386b5a3b5
Dmitriy <kazany.dmitry@gmail.com>
2022-09-05 00:05:43 +0300

    add task 27 sum digitals

---
+++ Task27_SumDig/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число ");
+string number = Console.ReadLine();
+
+int SumNumbers(string number)
+{   int result = 0;
+
+    for (int i = 0; i < number.Length; i++)
+    {
+        result += number[i] - '0';
+    }
+    
+    return result; 
+}
+Console.WriteLine("Сумма цифр данного числа равна " + SumNumbers(number));
